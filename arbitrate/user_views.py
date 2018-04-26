8f038f172a82bdaaa5fc325b5a6fe7f5058a4bfb
Ubuntu <ubuntu@ip-172-31-18-23.us-east-2.compute.internal>
2018-04-26 14:16:50 +0000

    change in api get data in required format

--- arbitrate/user_views.py
+++ arbitrate/user_views.py
@@ -2,13 +2,13 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import View
 import datetime
-# import pymongo
-# from pymongo import MongoClient
-# import os
-# import datetime
+import pymongo
+from pymongo import MongoClient
+import os
+import datetime
 
 
-# client = MongoClient('localhost', 27017)
+client = MongoClient('localhost', 27017)
 class TradeData(View):
 
     def get(self, request):
@@ -105,12 +105,14 @@ class TradeData(View):
 class GetData(View):
 
     def get(self, request):
-        # db = client['crypto_exchange']
-        # docs = db.exchange_data
-        # data = docs.find().sort('_id', pymongo.DESCENDING).limit(20)
+        db = client['crypto_exchange']
+        docs = db.exchange_data
+        data = docs.find().sort('_id', pymongo.DESCENDING).limit(20)
         query_data = []
-        # for each_data in data:
-        #     query_data.append(each_data)
+        for each_data in data:
+             each_data["_id"] = ""
+             query_data.append(each_data)
+        '''
         query_data = [{"_id": 1, "type": "strong", "bid_volume": 0.00150385, "date_time": "2018-03-14T12:36:49.219Z",
                    "bid": 0.12200001,
                    "ask": 0.11774, "ask_volume": 89.5129, "crypto_pair": "BCH/BTC",
@@ -120,4 +122,5 @@ class GetData(View):
                    "ask": 0.116201, "ask_volume": 4.483, "crypto_pair": "BCH/BTC", "spread": -0.005799010000000007,
                    "sell_here": "nova", "buy_here": "binance"}
                   ]
-        return JsonResponse()
+        '''
+        return JsonResponse(query_data, safe=False)
