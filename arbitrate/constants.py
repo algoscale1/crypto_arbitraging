0050e0eea12eed01fbe92737a0f17c385f8f011b
sandeep <sandeep.rawat@algoscale.com>
2018-04-27 11:28:26 +0530

    exhange list added

---
+++ arbitrate/constants.py
@@ -0,0 +1,12 @@
+EXCHANGES = ['allcoin', 'anxpro', 'binance', 'bitbay', 'bitcoincoid', 'bitfinex', 'bitfinex2', 'bitflyer', 'bitlish',
+             'bitmarket', 'bitso', 'bitstamp',
+             'bitstamp1', 'bittrex', 'bleutrade', 'braziliex', 'btcchina', 'btcexchange', 'btcmarkets', 'btctradeua',
+             'btcturk', 'ccex',
+             'cex', 'coinexchange', 'coingi', 'coinmate', 'cryptopia', 'dsx', 'exmo', 'gatecoin', 'gateio', 'gdax',
+             'gemini', 'hitbtc', 'hitbtc2', 'huobipro',
+             'kraken', 'kucoin', 'kuna', 'lakebtc', 'liqui', 'livecoin', 'mixcoins', 'nova', 'okex', 'qryptos',
+             'quadrigacx', 'quoinex', 'southxchange', 'therock',
+             'tidex', 'wex', 'yobit', 'zaif', 'zb']
+
+CRYPTO_PAIR = {'BCH/BTC', 'BTG/BTC', 'DASH/BTC', 'DOGE/BTC', 'ETH/BTC', 'LTC/BTC', 'NXT/BTC', 'PPC/BTC', 'SMS/BTC', 
+				'STRAT/BTC',  'XMR/BTC',  'XVG/BTC'}
