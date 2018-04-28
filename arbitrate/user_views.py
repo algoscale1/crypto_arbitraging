8eb342b24983932a72cf486b0842350643536823
shivtuls <stulsyan@algoscale.com>
2018-04-28 21:18:42 +0000

    db name correction

--- arbitrate/user_views.py
+++ arbitrate/user_views.py
@@ -12,8 +12,9 @@ crypto_pairs = CRYPTO_PAIRS
 client = MongoClient('localhost', 27017)
 class TradeData(View):
     def get(self, request):
-        db = client['crypt_exch']
-        data = db.exch_data.find({'type': 1}).sort('_id', pymongo.DESCENDING).limit(20)
+        db = client['crypto_exchange']
+        data = db.exchange_data.find({'type': 1}).limit(20)
+        print(data,"dataaaaaa")
         query_data = []
         for each_data in data:
             each_data["_id"] = ""
@@ -23,9 +24,8 @@ class TradeData(View):
 
 class GetExchanges(View):
     def get(self, request):
-
-        db = client['crypt_exch']
-        data = db.exch_data.find({'type': 1}).sort('_id', pymongo.DESCENDING).limit(20)
+        db = client['crypto_exchange']
+        data = db.exchange_data.find({'type': 1}).limit(20)
         query_data = []
         for each_data in data:
              each_data["_id"] = ""
@@ -36,7 +36,7 @@ class GetExchanges(View):
 
 import json
 class Filter(View):
-    @csrf_exempt
+    
     def post(self, request):
 
         post_data = request.POST
@@ -48,11 +48,12 @@ class Filter(View):
         sell = da['sell']
         pairs = da['pair']
 
-        db = client['crypt_exch']
-        data = db.exch_data.find({'type':1, 'buy_here':{'$in':buy}, 'sell_here': {'$in': sell},
+        db = client['crypto_exchange']
+        data = db.exchange_data.find({'type':1, 'buy_here':{'$in':buy}, 'sell_here': {'$in': sell},
         'crypto_pair': {'$in': pairs}}).limit(20)
         query_data = []
         for each_data in data:
+ 
              each_data["_id"] = ""
              query_data.append(each_data)
 
